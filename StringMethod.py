8e555326151a16ad4d8374e2c30143edb8546d50
anmolsingh7275 <anmolprogramming@gmail.com>
2026-01-20 12:58:50 +0530

    String method

---
+++ StringMethod.py
@@ -0,0 +1,13 @@
+name = "Bro"
+#Length Method 
+print(len(name))
+#Find Method  for index  of a charcater 
+# print(name.find("B"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace('o','a'))
+print(name*3)
---
+++ Variables.py
@@ -0,0 +1,4 @@
+#Print multiples assigmennt  variables in one line 
+
+name , age , attractive = "Alice" , 30 , True
+print(name,age,attractive)
