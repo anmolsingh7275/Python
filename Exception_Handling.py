b4fece5e966d4123f0d467da85c29c0a746ea486
anmolsingh7275 <anmolprogramming@gmail.com>
2026-02-27 23:46:15 +0530

    Practice

---
+++ Exception_Handling.py
@@ -0,0 +1,11 @@
+# x = int(input("Ener a number x: "))
+# y = int(input("Ener a number y: "))
+
+# try:
+#     result = x/y
+#     print(result)
+# except Exception:
+#     print("Something went wrong")    
+
+with open(r"C:\Users\Lenovo\Desktop\test.txt", "w") as file:
+    file.write("Hello world")
