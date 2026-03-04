09bae5d24b9f878e20f0ab2b98071bf5d8abde5a
anmolsingh7275 <anmolprogramming@gmail.com>
2026-03-04 21:10:39 +0530

    Logind and debugging and testing

---
+++ Unit_Testing/test_calculator.py
@@ -0,0 +1,13 @@
+import unittest
+from Calculator import add
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(-1, 1), 0)
